13a62bbf9eb71c5e071c7fc4d6a81363ac8444d2
vladcto <razrab.ytka@gmail.com>
2023-04-08 03:45:12 +0300

    Cделал считывание сообщений из БД.

--- lib/domain/provider/message_provider.dart
+++ lib/domain/provider/message_provider.dart
@@ -1,3 +1,12 @@
-class MessageProvider {
+import 'package:mock_chat_flutter/data/messages/message_dto.dart';
+import 'package:mock_chat_flutter/data/messages/service/firebase_message_service.dart';
+import 'package:mock_chat_flutter/data/messages/service/message_service.dart';
+import 'package:riverpod/riverpod.dart';
 
+class MessageProvider extends StateNotifier<List<MessageDTO>> {
+  final MessageService service;
+
+  MessageProvider(this.service, super.state) {
+    service.listenUpdates(() async => state = await service.getMessages());
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mock_chat_flutter/firebase_options.dart';
+import 'package:mock_chat_flutter/presentation/messages_page.dart';
 
-void main() {
+import 'locator.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  configureDependencies();
+
   runApp(const MainApp());
 }
 
@@ -10,11 +19,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: MessagesPage(),
     );
   }
 }
